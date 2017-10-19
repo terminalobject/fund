4053e9e8fd641f9833546b16b1c582372a50ff7e
Alistair Kung <alistaircckung@gmail.com>
2017-10-19 16:38:46 +0100

    layout bootstrapped

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,7 @@ var db = mongojs('mongodb://alistair:cokelife@ds121965.mlab.com:21965/fund')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  db.funds.find(function(err, funds){
-    if(err){
-      res.send(err);
-    }
-    res.json(funds);
-  });
+  res.render('index', {});
 });
 
 
