086d8ff63af5570239534697dfaaeb6548d4d036
Alistair Kung <alistaircckung@gmail.com>
2017-10-20 18:27:05 +0100

    html validations on form

--- app.js
+++ app.js
@@ -1,40 +1,83 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
 var expressValidator = require('express-validator');
+var expressLayouts = require('express-ejs-layouts');
 var flash = require('connect-flash');
 var session = require('express-session');
 var passport = require('passport')
-var LocalStrategy = require('passport-local'),Strategy;
+var LocalStrategy = require('passport-local').Strategy;
 var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://alistair:cokelife@ds121965.mlab.com:21965/fund');
+mongoose.Promise = global.Promise;
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 var fund = require('./routes/fund');
+
+// Init App
 var app = express();
-var expressLayouts = require('express-ejs-layouts');
-var cors = require('cors');
-mongoose.connect('mongodb://alistair:cokelife@ds121965.mlab.com:21965/fund');
-mongoose.Promise = global.Promise;
+
 // view engine setup
-app.use(expressLayouts);
-app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
+
+//BodyParser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.'),
+      root    = namespace.shift(),
+      formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
 
 app.use('/', index);
 app.use('/users', users);
 app.use('/fund', fund);
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -54,34 +97,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-// Express Validator
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
-
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
-    }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
-}));
-
-// Connect Flash
-app.use(flash());
-
-// Global Vars
-app.use(function (req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  res.locals.user = req.user || null;
-  next();
-});
 
 module.exports = app;
